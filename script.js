c86d2cc808ef184c7ee2e65bf4f8e0daaca6d4f7
thecla2312 <uisichie@gmail.com>
2023-06-13 14:23:28 +0100

    dom

---
+++ script.js
@@ -0,0 +1,45 @@
+/// SELect elementS on the webpage- class, id, tagName, css selectors
+
+// const heading = document.getElementsByClassName("heading");
+// console.log(heading);
+// const para = document.getElementById("ptag");
+// console.log(para);
+
+// const p = document.getElementsByTagName("p");
+// console.log(p);
+ 
+// //querySlector - css Selectors - .class, #id, p
+// querySelectoraALL
+
+// const paragragh = document.querySelector("p");
+// console.log(paragragh);
+// const paragraghs = document.querySelectorAll("p");
+// paragraghs.forEach((p) => console.log(p));
+// const hi = document.querySelector("heading");
+// console.log(hi);
+
+// const div = document.querySelector("#test")
+// console.log(div);
+
+// // changing content on the web - innerText, textContent, innerHTMl
+// console.log(h1.innerText);
+// h1, innerText += "Welcome";
+// console.log(h1.textContent);
+// hi, textContent = "Hello class";
+// console.log(div.innerHTML);sp
+// div
+// div,innerHTML +=  "<a>visit google</a>"
+    
+ =
+//select the body
+//h1 javascript
+//bacground color of blue
+
+
+const body = document.querySelector('body')
+body.innerHTML = '<h1>JAVASCRIPT</h1>'
+body.style.backgroundColor = 'blue'
+
+const success = document.querySelector("success");
+success.classList.remove(test);
+
